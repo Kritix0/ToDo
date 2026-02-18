d1d941292c222f7d68791549265da0c49aa436d5
Kritix0 <ramazdem098@gmail.com>
2026-02-18 19:17:25 +0700

    added funstion of todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+
+type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  // Стейт для списка задач
+  const [todos, setTodos] = useState<Todo[]>([]);
+  
+  // Стейт для инпута
+  const [inputValue, setInputValue] = useState("");
+
+  // Функция добавления
+  function handleAdd() {
+    // Проверяем что не пусто
+    if (inputValue.trim() === "") {
+      return;
+    }
+
+    // Создаём объект, который соответствует типу Todo
+    const newTodo: Todo = {
+      id: Date.now(), // уникальный id (текущее время в миллисекундах)
+      text: inputValue,
+      completed: false,
+    };
+
+    setTodos([...todos, newTodo]);
+    setInputValue(""); // очищаем инпут после добавления
+  }
+
+
+  // Функция переключения completed
+  function handleToggle(id: number) {
+    setTodos(todos.map(todo => 
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ))
+  }
+
+  // Функция удаления
+  function handleDelete(id: number) {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <input 
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+        placeholder="Введи задачу"
+      />
+      <button onClick={handleAdd}>Добавить</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+          <span 
+            onClick={() => handleToggle(todo.id)}
+            style={{ 
+              textDecoration: todo.completed ? "line-through" : "none",
+              cursor: "pointer"
+            }}
+          >
+            {todo.text}
+          </span>
+            <button onClick={() => handleDelete(todo.id)}>Удалить</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
